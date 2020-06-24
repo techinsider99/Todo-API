c227187216c5107d81538039039d76ce15992218
Saran RTS <rtssaran@gmail.com>
2020-06-24 22:17:36 +0530

    Updates

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use('/api/todo', todoHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT}`)
+    console.log(`Server is up and running on port: ${PORT}`)
 })
